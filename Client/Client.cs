01e19ec458fe382db5230c800b7672b81816185d
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2021-12-27 21:17:36 +0300

    part2

---
+++ Client/Client.cs
@@ -0,0 +1,85 @@
+﻿using OS4_p2;
+using OS4_p2.WinApi;
+using System;
+using System.Text;
+using System.Threading;
+using static OS4_p2.WinApi.Functions;
+namespace Client
+{
+	class Client
+	{
+		static Menu mainMenu = new Menu("Client",
+			new IMenuItem[] {
+				new MenuItem("Connect pipe", ConnectPipe),
+				new MenuItem("Receive message", GetMessage),
+				new MenuItem("Disconnect from pipe", DisconnectPipe)
+			});
+
+		static NativeOverlapped overlapped;
+		static IntPtr evt;
+		static IntPtr pipe;
+		static uint bufSz = 512;
+		static unsafe IOCompletionCallback completionCallback;
+		static bool connected = false;
+
+		static unsafe void Callback(uint errCode, uint bytes, NativeOverlapped* ov)
+		{
+			Console.WriteLine("Data received!");
+		}
+		static unsafe void ConnectPipe()
+		{
+			if (completionCallback == null)
+				completionCallback += Callback;
+
+		
+
+			pipe = CreateFile(@"\\.\pipe\mypipe",
+				(uint)DesiredAccess.GENERIC_READ | (uint)DesiredAccess.GENERIC_WRITE,
+				(uint)ShareMode.None,
+				null,
+				(uint)CreationDisposition.OPEN_EXISTING,
+				(uint)PipeOpenModeFlags.FILE_FLAG_OVERLAPPED,
+				IntPtr.Zero);
+			connected = true;
+		}
+		static void GetMessage()
+		{
+			if (!connected)
+			{
+				Console.WriteLine("You need to connect pipe at first!");
+				return;
+			}
+			
+			byte[] buf = new byte[bufSz];
+			var res = ReadFileEx(pipe, buf, bufSz, ref overlapped, completionCallback);
+			SleepEx(Constants.INFINITE, true);
+			if (res)
+			{
+				string str = Encoding.UTF8.GetString(buf);
+				str = str.Trim('\0');
+				Console.WriteLine(str);
+			}
+			else
+				Console.WriteLine($"Error occured while reading!. Code: {GetLastError()}");
+		}
+		static void DisconnectPipe()
+		{
+			if (!connected)
+			{
+				Console.WriteLine("You need to connect pipe at first!");
+				return;
+			}
+			if (CloseHandle(pipe))
+				Console.WriteLine("Disconnection successful");
+			else
+				Console.WriteLine($"Error occured. Code: {GetLastError()}");
+			connected = false;
+		}
+
+		static void Main(string[] args)
+		{
+			mainMenu.Select();
+			
+		}
+	}
+}
---
+++ Server/Server.cs
@@ -0,0 +1,95 @@
+﻿using OS4_p2;
+using OS4_p2.WinApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static OS4_p2.WinApi.Functions;
+
+namespace Server
+{
+    class Server
+    {
+		static Menu mainMenu = new Menu("Server",
+			   new IMenuItem[] {
+				new MenuItem("Create named", CreatePipe),
+				new MenuItem("Send ", SendMessage),
+				new MenuItem("Disconnect", DisconnectPipe)
+			   });
+
+		static bool pipeCreated = false;
+		static uint outBufSz = 512, inBufSz = 512;
+		static IntPtr pipe;
+		static NativeOverlapped overlapped = new NativeOverlapped();
+		static IntPtr evt;
+		unsafe static void CreatePipe()
+		{
+			evt = CreateEvent(IntPtr.Zero, false, false, "myevt");
+			pipe = CreateNamedPipe("\\\\.\\pipe\\mypipe",
+				(uint)PipeOpenModeFlags.PIPE_ACCESS_DUPLEX,
+				(uint)PipeModeFlags.PIPE_TYPE_MESSAGE | (uint)PipeModeFlags.PIPE_READMODE_MESSAGE | (uint)PipeModeFlags.PIPE_WAIT,
+				Constants.PIPE_UNLIMITED_INSTANCES,
+				outBufSz,
+				inBufSz,
+				0,
+				null);
+
+			NativeOverlapped syncPipe = new NativeOverlapped() { EventHandle = evt };
+
+			var res = ConnectNamedPipe(pipe, ref syncPipe);
+
+			WaitForSingleObject(evt, Constants.INFINITE);
+
+			if (res)
+			{
+				Console.WriteLine("Pipe created successfully");
+				pipeCreated = true;
+			}
+			else
+				Console.WriteLine($"Error creating pipe! Error {GetLastError()}");
+		}
+
+		static unsafe void SendMessage()
+		{
+			if (!pipeCreated)
+			{
+				Console.WriteLine("Please, create pipe");
+				return;
+			}
+
+			Console.WriteLine("Input message:");
+			string str = Console.ReadLine();
+			var outputStr = Encoding.UTF8.GetBytes(str);
+			Array.Resize(ref outputStr, (int)outBufSz);
+
+			fixed (NativeOverlapped* o = &overlapped)
+			{
+				var res = WriteFile(pipe, outputStr, outBufSz, out uint written, o);
+				if (res)
+					Console.WriteLine("Message received successfully");
+				else
+					Console.WriteLine($"Error writing message. Error  {GetLastError()}");
+			}
+		}
+
+		static void DisconnectPipe()
+		{
+			if (!pipeCreated)
+			{
+				Console.WriteLine("Please, create pipe ");
+				return;
+			}
+			if (DisconnectNamedPipe(pipe))
+				Console.WriteLine("Disconnected!");
+			else
+				Console.WriteLine($"Error disconnecting pipe! Error {GetLastError()}");
+			pipeCreated = false;
+		}
+		static void Main(string[] args)
+		{
+			mainMenu.Select();
+		}
+	}
+}
